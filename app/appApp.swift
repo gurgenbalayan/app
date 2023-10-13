e3fa943863d3f02c13c0cf0532833ff33c2b10fe
g.balayan <g.balayan@notamedia.ru>
2023-10-13 18:15:57 +0300

    Initial Commit

---
+++ app/appApp.swift
@@ -0,0 +1,17 @@
+//
+//  appApp.swift
+//  app
+//
+//  Created by notamedia on 13.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
